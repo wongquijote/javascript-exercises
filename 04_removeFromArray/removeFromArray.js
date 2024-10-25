fdbcfefd1e864bfaf6e41e2403e2891c5d0d6c10
wongquijote <goingtokyle@gmail.com>
2024-10-25 12:04:31 -0700

    did some js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeWords) { // rest parameters gives variable args
+    // for ... in iterates over all enumerable properties of an object
+    // for ... of interates over all enumerable values of an object
+    for (const word of removeWords) { 
+        while (arr.indexOf(word) !== -1) {
+            arr.splice(arr.indexOf(word), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
